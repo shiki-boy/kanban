941309d61d47d8b5fc6baad56b8c470dcf608806
Raghav Kaushik <raghav.kaushik611@gmail.com>
2024-04-30 22:49:36 +0530

    drag drop logic working

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -4,39 +4,69 @@
 import { cn } from '@/lib/utils'
 import { useState } from 'react'
 
-const data = {
+const DATA = {
   todo: [ 6, 7 ],
   'in-progress': [ 1, 2, 3 ],
   done: [ 4, 5 ],
 }
 
 export default function Tasks() {
-  const onDrop = ( col: any, i: number ) => {}
+  const [ data, setData ] = useState( DATA )
+  const [ draggedCard, setDraggedCard ] = useState<null | {
+    col: keyof typeof DATA;
+    id: number;
+  }>( null )
+
+  const onDrop = ( colTarget: keyof typeof DATA, iTarget: number ) => {
+    if ( !draggedCard ) return
+
+    const _data = { ...data }
+
+    // remove
+    _data[draggedCard.col] = _data[draggedCard.col].filter(
+      ( e ) => e !== draggedCard.id,
+    )
+
+    // add
+    _data[colTarget].splice( iTarget, 0, draggedCard.id )
+
+    setData( _data )
+  }
 
   return (
     <>
       <div className='flex h-full gap-2'>
-        {Object.keys( data ).map( ( column, ci ) => (
-          <div className='w-[200px] h-full bg-green-200 p-4' key={ ci }>
-            <p className='mb-4 font-bold w-full'>{column}</p>
+        {( Object.keys( data ) as unknown as Array<keyof typeof data> ).map(
+          ( column, ci ) => (
+            <div className='w-[200px] h-full bg-green-200 p-4' key={ ci }>
+              <p className='mb-4 font-bold w-full'>{column}</p>
 
-            <div className='flex flex-col gap-2'>
-              {data[column as keyof typeof data].map( ( card ) => (
-                <>
-                  <DropArea onDrop={ () => onDrop( column, ci ) } />
-                  <div
-                    draggable='true'
-                    onDragStart={ () => {} }
-                    className='card cursor-grab active:cursor-grabbing active:animate-pulse h-[100px] bg-green-100 rounded-md p-2 grid place-items-center'
-                    key={ `-${ card }` }
-                  >
-                    {card}
-                  </div>
-                </>
-              ) )}
+              <DropArea onDrop={ () => onDrop( column, 0 ) } />
+
+              <div className='flex flex-col gap-2'>
+                {data[column].map( ( card, i ) => (
+                  <>
+                    <div
+                      draggable='true'
+                      onDragStart={ () =>
+                        setDraggedCard( {
+                          col: column,
+                          id: card,
+                        } )
+                      }
+                      className='card cursor-grab active:cursor-grabbing active:animate-pulse h-[100px] bg-green-100 rounded-md p-2 grid place-items-center'
+                      key={ `-${ card }` }
+                    >
+                      {card}
+                    </div>
+
+                    <DropArea onDrop={ () => onDrop( column, i + 1 ) } />
+                  </>
+                ) )}
+              </div>
             </div>
-          </div>
-        ) )}
+          ),
+        )}
       </div>
     </>
   )
